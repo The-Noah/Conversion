3bf98594e207832a3e15260e4e237339923d37e1
The Noah <thenoahbz@gmail.com>
2019-07-27 16:22:13 -0500

    Add millimeter to length converter

--- Converters/LengthConverter.cs
+++ Converters/LengthConverter.cs
@@ -2,7 +2,7 @@
 
 namespace Conversion.Converters{
   public class LengthConverter : Converter {
-    public override string[] measurements => new string[]{"Inch", "Centimeter", "Foot"};
+    public override string[] measurements => new string[]{"Inch", "Centimeter", "Foot", "Millimeter"};
     public override string name => "Length";
 
     public override void Convert(TextBox fromTextBox, TextBox toTextBox, int fromIndex, int toIndex){
@@ -30,6 +30,18 @@ namespace Conversion.Converters{
         to = from * 12;
       }else if(fromIndex == 2 && toIndex == 1){ // foot -> cm
         to = from * 30.48;
+      }else if(fromIndex == 0 && toIndex == 3){ // inch -> mm
+        to = from * 2.54 * 10.0;
+      }else if(fromIndex == 1 && toIndex == 3){ // cm -> mm
+        to = from * 10.0;
+      }else if(fromIndex == 2 && toIndex == 3){ // foot -> mm
+        to = from * 304.8;
+      }else if(fromIndex == 3 && toIndex == 0){ // mm -> inch
+        to = from / 25.4;
+      }else if(fromIndex == 3 && toIndex == 1){ // mm -> cm
+        to = from / 10.0;
+      }else if(fromIndex == 3 && toIndex == 2){ // mm -> foot
+        to = from / 304.8;
       }
 
       toTextBox.Text = to.ToString();
