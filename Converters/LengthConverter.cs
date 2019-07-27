28b52e03f9d9bda30b7fcc8ec3583378b8a07d20
The Noah <thenoahbz@gmail.com>
2019-07-27 16:08:16 -0500

    Add foot to length converter

--- Converters/LengthConverter.cs
+++ Converters/LengthConverter.cs
@@ -2,7 +2,7 @@
 
 namespace Conversion.Converters{
   public class LengthConverter : Converter {
-    public override string[] measurements => new string[]{"Inch", "Centimeter"};
+    public override string[] measurements => new string[]{"Inch", "Centimeter", "Foot"};
     public override string name => "Length";
 
     public override void Convert(TextBox fromTextBox, TextBox toTextBox, int fromIndex, int toIndex){
@@ -22,6 +22,14 @@ namespace Conversion.Converters{
         to = from * 2.54;
       }else if(fromIndex == 1 && toIndex == 0){ // cm -> inch
         to = from / 2.54;
+      }else if(fromIndex == 0 && toIndex == 2){ // inch -> foot
+        to = from / 12.0;
+      }else if(fromIndex == 1 && toIndex == 2){ // cm -> foot
+        to = from / 30.48;
+      }else if(fromIndex == 2 && toIndex == 0){ // foot -> inch
+        to = from * 12;
+      }else if(fromIndex == 2 && toIndex == 1){ // foot -> cm
+        to = from * 30.48;
       }
 
       toTextBox.Text = to.ToString();
