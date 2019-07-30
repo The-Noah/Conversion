20894adea417454b05dc8e77d4bb30c1a767aa17
The Noah <thenoahbz@gmail.com>
2019-07-30 15:42:49 -0500

    Reorder length conversion units

--- Converters/LengthConverter.cs
+++ Converters/LengthConverter.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace Conversion.Converters{
   public class LengthConverter : Converter{
-    public override string[] measurements => new string[]{"Inch", "Centimeter", "Foot", "Millimeter", "Mile", "Kilometer", "Yard", "Meter"};
+    public override string[] measurements => new string[]{"Inch", "Foot", "Yard", "Mile", "Millimeter", "Centimeter", "Meter", "Kilometer"};
     public override string name => "Length";
 
     private const double INCH_TO_METER = .0254;
-    private const double CM_TO_METER = .01;
     private const double FOOT_TO_METER = .3048;
-    private const double MM_TO_METER = .001;
+    private const double YARD_TO_METER = .9144;
     private const double MILE_TO_METER = 1609.344;
+    private const double MM_TO_METER = .001;
+    private const double CM_TO_METER = .01;
     private const double KM_TO_METER = 1000.0;
-    private const double YD_TO_METER = .9144;
 
     public override void Convert(TextBox fromTextBox, TextBox toTextBox, int fromIndex, int toIndex){
       if(fromIndex == toIndex){
@@ -32,19 +31,19 @@ namespace Conversion.Converters{
       if(index == 0){
         return INCH_TO_METER;
       }else if(index == 1){
-        return CM_TO_METER;
-      }else if(index == 2){
         return FOOT_TO_METER;
+      }else if(index == 2){
+        return YARD_TO_METER;
       }else if(index == 3){
-        return MM_TO_METER;
-      }else if(index == 4){
         return MILE_TO_METER;
+      }else if(index == 4){
+        return MM_TO_METER;
       }else if(index == 5){
-        return KM_TO_METER;
+        return CM_TO_METER;
       }else if(index == 6){
-        return YD_TO_METER;
-      }else if(index == 7){
         return 1.0;
+      }else if(index == 7){
+        return KM_TO_METER;
       }
 
       return 0.0;
