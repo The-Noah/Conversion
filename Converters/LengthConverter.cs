0051ef71a461429d8876e5a930fbda80714550f6
The Noah <thenoahbz@gmail.com>
2019-07-30 15:38:49 -0500

    Add mile, kilometer, yard, and meter to length converter

--- Converters/LengthConverter.cs
+++ Converters/LengthConverter.cs
@@ -1,14 +1,18 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 
 namespace Conversion.Converters{
   public class LengthConverter : Converter{
-    public override string[] measurements => new string[]{"Inch", "Centimeter", "Foot", "Millimeter"};
+    public override string[] measurements => new string[]{"Inch", "Centimeter", "Foot", "Millimeter", "Mile", "Kilometer", "Yard", "Meter"};
     public override string name => "Length";
 
     private const double INCH_TO_METER = .0254;
     private const double CM_TO_METER = .01;
     private const double FOOT_TO_METER = .3048;
     private const double MM_TO_METER = .001;
+    private const double MILE_TO_METER = 1609.344;
+    private const double KM_TO_METER = 1000.0;
+    private const double YD_TO_METER = .9144;
 
     public override void Convert(TextBox fromTextBox, TextBox toTextBox, int fromIndex, int toIndex){
       if(fromIndex == toIndex){
@@ -33,9 +37,17 @@ namespace Conversion.Converters{
         return FOOT_TO_METER;
       }else if(index == 3){
         return MM_TO_METER;
+      }else if(index == 4){
+        return MILE_TO_METER;
+      }else if(index == 5){
+        return KM_TO_METER;
+      }else if(index == 6){
+        return YD_TO_METER;
+      }else if(index == 7){
+        return 1.0;
       }
 
-      return 1.0;
+      return 0.0;
     }
   }
 }
